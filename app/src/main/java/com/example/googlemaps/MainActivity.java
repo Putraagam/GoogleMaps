b8b95c2ad7dff897f369471bbf74b85dfe68c81d
Putraagam <64558982+Putraagam@users.noreply.github.com>
2020-04-30 14:26:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.googlemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+
+        LatLng Blitar = new LatLng(-8.088743, 112.155434);
+        map.addMarker(new MarkerOptions().position(Blitar).title("SPBU Pertamina 54.661.34 (POM TANJUNGSARI)"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Blitar));
+    }
+}
